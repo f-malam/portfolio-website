8d08dda6d548b5bb2773fe779d241346fdcf0dad
Fatema Malam <fmalam@hotmail.co.uk>
2018-09-04 11:26:09 -0700

    code linters

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
   /* Smooth Scroll animation */
   // Select all links with hashes
-  $('a[href*="#"]')
+$('a[href*="#"]')
   // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
+.not('[href="#"]')
+.not('[href="#0"]')
+  .click(function (event){
     // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
@@ -28,9 +28,9 @@ $(document).ready(function(){
   });
 
   /* on scroll animation script */
-  $(window).scroll(function() {
-    $(".animation").each(function(){
-      var imagePos = $(this).offset().top;
+$(window).scroll(function(){
+$('.animation').each(function(){
+var imagePos = $(this).offset().top;
       var topOfWindow = $(window).scrollTop();
       if (imagePos < topOfWindow + 300) {
         $(this).addClass("fade");
@@ -41,29 +41,29 @@ $(document).ready(function(){
 });
 
 /* Slideshow script */
-var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"]
-var dotId = ["dot", "dot2"]
-showSlides(1, 0);
-showSlides(1, 1);
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+var slideIndex = [1, 1]
+var slideId = ['mySlides1', 'mySlides2']
+var dotId = ['dot', 'dot2']
+showSlides(1, 0)
+showSlides(1, 1)
+function plusSlides (n, no) {
+  showSlides(slideIndex[no] += n, no)
 }
-function currentSlide(n, no) {
-  showSlides(slideIndex[no] = n, no);
+function currentSlide (n, no) {
+  showSlides(slideIndex[no] = n, no)
 }
-function showSlides(n, no) {
-  var i;
-  var slides = document.getElementsByClassName(slideId[no]);
-  var dots = document.getElementsByClassName(dotId[no]);
-  if (n > slides.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = slides.length}
+function showSlides (n, no) {
+  var i
+  var slides = document.getElementsByClassName(slideId[no])
+  var dots = document.getElementsByClassName(dotId[no])
+  if (n > slides.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = ' none'
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(' active', '')
   }
-  slides[slideIndex[no]-1].style.display = "block";
-  dots[slideIndex[no]-1].className += " active";
+  slides[slideIndex[no]-1].style.display = ' block'
+  dots[slideIndex[no]-1].className += ' active'
 }
