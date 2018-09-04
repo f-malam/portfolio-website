9cf90240f8d053a80dd6366ecefaefc3df6bc744
Fatema Malam <fmalam@hotmail.co.uk>
2018-09-03 17:57:21 -0700

    added images

--- js/script.js
+++ js/script.js
@@ -2,60 +2,56 @@ $(document).ready(function(){
   /* Smooth Scroll animation */
   // Select all links with hashes
   $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-          });
-        }
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+        });
       }
-    });
+    }
+  });
 
-  /* Scroll animation script */
+  /* on scroll animation script */
   $(window).scroll(function() {
-    $('.animation').each(function(){
+    $(".animation").each(function(){
       var imagePos = $(this).offset().top;
       var topOfWindow = $(window).scrollTop();
       if (imagePos < topOfWindow + 300) {
-        $(this).addClass("bounce");
+        $(this).addClass("fade");
       }
     });
   });
-  $('.element').css('visibility', 'hidden');
+  $(".element").css("visibility", "hidden");
 });
 
 /* Slideshow script */
-
 var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2"]
 var dotId = ["dot", "dot2"]
 showSlides(1, 0);
 showSlides(1, 1);
-
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
-
 function currentSlide(n, no) {
   showSlides(slideIndex[no] = n, no);
 }
-
 function showSlides(n, no) {
   var i;
   var slides = document.getElementsByClassName(slideId[no]);
@@ -71,5 +67,3 @@ function showSlides(n, no) {
   slides[slideIndex[no]-1].style.display = "block";
   dots[slideIndex[no]-1].className += " active";
 }
-
-/* parallax effect */
